b6afbadaceb3889ada47211f863b50ba58c5f488
Saadaq12 <saadaq.mohamudali@student.nbi-handelsakademin.se>
2025-12-31 09:45:18 +0100

    steg 2

--- obj/Debug/net9.0/cyberarcade.AssemblyInfo.cs
+++ obj/Debug/net9.0/cyberarcade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cyberarcade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3209dd9dda179bbdb0208e38ed7bafce840d9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("cyberarcade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cyberarcade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
